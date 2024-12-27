e2659362942b5441bcefcde540134374b8ef3a16
Memnoc <smartdroidesign@gmail.com>
2024-12-27 16:37:58 +0000

    custom todo HEADER and updates to core

---
+++ lua/plugins/todo.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      keywords = {
+        HEADER = { icon = " ", color = "header" },
+      },
+      colors = {
+        header = { "#ff9e64" },
+      },
+    },
+  },
+}
