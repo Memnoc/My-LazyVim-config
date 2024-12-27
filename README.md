5c82ae772be33c6da1a046bc843b27a831bd1a09
Memnoc <smartdroidesign@gmail.com>
2024-12-27 16:26:39 +0000

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# 💤 LazyVim - Memnoc config
+
+Handy way to keep track of my own LazyVim config throughout dev environments and machines
+
+### Credits
+
+Thanks to [LazyVim](https://github.com/LazyVim/LazyVim).
+Official docs [documentation](https://lazyvim.github.io/installation) to get started.
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup({
+        direction = "horizontal",
+        size = 15,
+        persist_size = false,
+      })
+    end,
+    keys = {
+      {
+        "<Leader>9",
+        desc = "Toggle Terminal",
+        function()
+          require("toggleterm").toggle(vim.v.count1)
+        end,
+      },
+    },
+    lazy = false,
+  },
+}
